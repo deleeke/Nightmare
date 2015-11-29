4733dc03410ab7a59ee474473c23840e92f7aeec
Elijah <deleeke@gmail.com>
2015-11-29 10:12:07 -0500

    Created new function to run scenes

--- rough_draft.py
+++ rough_draft.py
@@ -55,60 +55,58 @@ class Scene(object):
             if self.txt[i] != None:
                 print self.txt[i]
                 i += 1
-                sleep(2)
+                sleep(4)
         pass
 
-#lives = [1,2,3]
-#usr = NewUsr('Laura Kraft', 30)
+def ExeScene(scenename):
+    scenename.printslow(scenename.storytxt, 0,5)
+    usrinput = raw_input('what do you do? > ').lower()
+    usrinput = usrinput.split(' ')
+    keywords = False
+    for word in usrinput:
+        for i in range(0,len(usrAction)):
+            if word in usrAction[i]:
+                keywords = True
+                print aiLines[i][random.randint(0,(len(aiLines[i][:])-1))]
+                sleep(3)
+                print BUZZER
+        if keywords == False:
+            print scenename.printline(scenename.storytxt, -1)
+            sleep(2)
+            print scenename.randline(scenename.usrtxt)
+            sleep(2)
+            scenename.printslow(scenename.failtxt, 0, -1)
 
 runAway = AIDict()
 runAway.importData('usrRun.txt','aiRunresponse.txt')
 attack = AIDict()
 attack.importData('usrAttack.txt', 'aiAttackresponse.txt')
+love = AIDict()
+love.importData('usrLove.txt', 'aiLoveResponse.txt')
 
-usrAction = [attack.usrKeywords, runAway.usrKeywords]
-aiLines = [attack.aiResponse, runAway.aiResponse]
+usrAction = [attack.usrKeywords, runAway.usrKeywords, love.usrKeywords]
+aiLines = [attack.aiResponse, runAway.aiResponse, love.aiResponse]
 
 start = Scene('mainentry.txt','','')
 date = Scene('dateEntry.txt', 'quotes.txt','datefail.txt')
 job = Scene('','','')
 fridge = Scene('','','')
 
+BUZZER = '\n\n*******bbbbzzzzzzzzzz*******\n\n'
 
 def gameloop(usr, lives):
     for spawn in lives:
-        print 'bbbbzzzzzzzzzz\n\n\n'
+        print BUZZER
         start.printslow(start.storytxt,0,-1)
-        choice1 = ''
-        while choice1 not in ['1', '2', '3']:
-            choice1 = str(raw_input("Pick a number 1 to 3\n> "))
-        if choice1 == '1':
-            date.printslow(date.storytxt, 0,5)
-            usrinput = raw_input('what do you do? > ').lower()
-            usrinput = usrinput.split(' ')
-            keywords = False
-            for word in usrinput:
-                for i in range(0,len(usrAction)):
-                    #print 'in the loop for the %r time' % i
-                    if word in usrAction[i]:
-                        keywords = True
-                        #print 'usrinput in usrAction[%r] True' % i     
-                        print aiLines[i][random.randint(0,(len(aiLines[i][:])-1))]
-                        #print 'broke out of inner loop'
-                        sleep(3)
-                        print 'bbbbzzzzzzzzzz\n\n\n'
-            if keywords == False:
-                print date.printline(date.storytxt, -1)
-                sleep(2)
-                print date.randline(date.usrtxt)
-                sleep(2)
-                date.printslow(date.failtxt, 0, -1)
-        elif choice1 == '2':
-            print 'job scene'
-            pass 
+        choice = ''
+        while choice not in ['1', '2', '3']:
+            choice = str(raw_input("Pick a number 1 to 3\n> "))
+        if choice == '1':
+            ExeScene(date)
+        elif choice == '2':
+            ExeScene(job)
         else:
-            print 'fridge scene'
-            pass
+            ExeScene(fridge)
 
     print """
     You soar into a seething maelstrom of particles...
@@ -124,18 +122,19 @@ Welcome to NIGHTMARE()
 It will be unpleasant...
 """
 name = str(raw_input("Enter Usr Name > "))
-age = 0
+age = '0'
 while int(age) not in range(1, 200):
     age = str(raw_input("Enter your age > "))
     if int(age) < 18:
         print "Sorry this game is not age appropriate."
         sys.exit(0)
-lives = 0
-while int(lives) not in range(1,11):
-    lives = raw_input("""
+lives = '0'
+DIGITS = range(1,11)
+while int(lives) not in DIGITS:
+    lives = str(raw_input("""
     On a scale from one to ten,\n
     how terrible would you like this game to be?
-    \n (10 being really terrible) > """)
+    \n (10 being really terrible) > """))
     
 lives = range(1, int(lives))
 gameloop(name, lives)
